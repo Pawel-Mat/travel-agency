01c0fd3c7e3beeca8f600a15625c1bf515f3c2b4
Paweł Matysiak <mat.pawel@gmail.com>
2020-08-20 20:07:06 +0200

    Fix trip filters

--- travel-agency/src/redux/filtersRedux.js
+++ travel-agency/src/redux/filtersRedux.js
@@ -10,11 +10,20 @@ const createActionName = name => `app/${reducerName}/${name}`;
 
 // action types
 export const CHANGE_PHRASE = createActionName('CHANGE_PHRASE');
+
 // TODO - add other action types
+export const ADD_TAG = createActionName('ADD_TAG');
+export const REMOVE_TAG = createActionName('REMOVE_TAG');
+export const DURATION_TO = createActionName('DURATION_TO');
+export const DURATION_FROM = createActionName('DURATION_FROM');
 
 // action creators
 export const changeSearchPhrase = payload => ({ payload, type: CHANGE_PHRASE });
 // TODO - add other action creators
+export const addTag = payload => ({payload, type: ADD_TAG});
+export const removeTag = payload => ({payload, type: REMOVE_TAG});
+export const durationTo = payload => ({payload, type: DURATION_TO});
+export const durationFrom = payload => ({payload, type: DURATION_FROM});
 
 // reducer
 export default function reducer(statePart = [], action = {}) {
@@ -23,6 +32,32 @@ export default function reducer(statePart = [], action = {}) {
       return {
         ...statePart,
         searchPhrase: action.payload,
+      };
+    case ADD_TAG:
+      return {
+        ...statePart,
+        tags: [...statePart.tags, action.payload],
+      };
+    case REMOVE_TAG:
+      return {
+        ...statePart,
+        tags: [...statePart.tags.filter((tag) => tag !== action.payload)],
+      };
+    case DURATION_TO:
+      return {
+        ...statePart,
+        duration: {
+          ...statePart.duration,
+          to: action.payload,
+        }, 
+      };
+    case DURATION_FROM:
+      return {
+        ...statePart,
+        duration: {
+          ...statePart.duration,
+          from: action.payload,
+        },
       };
     // TODO - handle other action types
     default:
